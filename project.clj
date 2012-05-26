ee172c3089f6d5bf31686582103f510ca2c5307d
Robin Kraft <robin@whizbangsystems.net>
2012-05-26 10:26:18 -0700

    changed defproject

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spatialog/spatialog "0.1.0-SNAPSHOT"
+(defproject net.robinkraft/spatialog "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :repositories {"conjars" "http://conjars.org/repo/"}
   :source-path "src/clj"
